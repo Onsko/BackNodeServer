2ca598b0591c7817ea5cba50dfb9581852407e58
Onsko <ons.kochtane@esen.tn>
2025-07-10 23:45:22 +0200

    teba3 champ role admin

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,17 @@
- import mongoose from "mongoose";
+import mongoose from "mongoose";
 
- const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    verifyOtp:{type:String,default:''},
-    verifyOtpExpireAt:{type:Number,default:0},
-    isAccountVerified:{type:Boolean,default:false},
-    resetOtp:{type:String, default:''},
-    resetOtpExpireAt:{type:Number, default:0},
- })
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verifyOtp: { type: String, default: '' },
+  verifyOtpExpireAt: { type: Number, default: 0 },
+  isAccountVerified: { type: Boolean, default: false },
+  resetOtp: { type: String, default: '' },
+  resetOtpExpireAt: { type: Number, default: 0 },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },  // <-- AJOUT DU ROLE ICI
+});
 
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
 
- const userModel=mongoose.models.user || mongoose.model('user',userSchema);
-
- export default userModel;
+export default userModel;
